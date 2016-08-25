789f1937f1bcf231204a190dfc7616bab952dc14
Emily Abrash <emabrash@gmail.com>
2016-08-25 10:34:58 -0700

    Implemented stack for Ruby

--- Stack.rb
+++ Stack.rb
@@ -2,19 +2,24 @@ class Stack
   def initialize
     @store = Array.new
   end
-  
+
   def push(element)
+    @store << element
   end
- 
+
   def pop
+    return @store.delete_at(-1)
   end
 
   def top
+    return @store.last
   end
- 
+
   def size
+    return @store.length
   end
 
   def empty?
+    return @store.length == 0
   end
 end
