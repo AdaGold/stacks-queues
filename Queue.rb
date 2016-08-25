b4e43588ccf7ffc57062e7c11e495cf7c2309280
jm-rives <joanna.rives@gmail.com>
2016-08-25 10:44:59 -0700

    ADD virtual Queue

--- Queue.rb
+++ Queue.rb
@@ -1,19 +1,26 @@
 class Queue
   def initialize
+    @queue = queue
   end
   
   def enqueue(element)
+    return @queue.push(element)
   end
  
   def dequeue
+   return  @queue.shift(element)
   end
 
   def front
+    return @queue.first
+
   end
  
   def size
+    return @queue.length
   end
 
   def empty?
+    return size == 0
   end
 end
