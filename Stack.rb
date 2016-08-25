132bf9d79347c3eb7050c1ab286e140d0f4830fb
Kelly Tran <trankphuong@gmail.com>
2016-08-25 10:35:01 -0700

    completed Implementation of Stack

--- Stack.rb
+++ Stack.rb
@@ -1,20 +1,27 @@
+# implementing a Stack : using rules of the Stack class. We created the rules.
+
 class Stack
   def initialize
     @store = Array.new
   end
-  
+
   def push(element)
+    @store.push(element) # @store << element
   end
- 
+
   def pop
+    return @store.pop
   end
 
   def top
+    return @store.last
   end
- 
+
   def size
+    return @store.length
   end
 
   def empty?
+    return size == 0 # return a boolean # @store.empty? # @store.length == 0
   end
 end
