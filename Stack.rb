eb2d1bede26f9ca9805133f568414bca5c554b8e
Maya Wang <salome.neptune@gmail.com>
2016-08-28 02:53:43 -0700

    add to_s method

--- Stack.rb
+++ Stack.rb
@@ -2,19 +2,31 @@ class Stack
   def initialize
     @store = Array.new
   end
-  
+
   def push(element)
-  end
- 
-  def pop
+    @store.push(element) # @store << element, element goes to the end of array.
   end
 
-  def top
+  def pop # take out and return the last element of the array
+    return @store.pop # @store.delete_at(-1)
   end
- 
+
+  def top # return the last element of the array
+    return @store.last
+  end
+
   def size
+    return @store.length
   end
 
   def empty?
+    return size == 0 # call size method
+    # store.empty?
+    # @store.length == 0
   end
+
+  def to_s
+    return @store.join(", ")
+  end
+
 end
